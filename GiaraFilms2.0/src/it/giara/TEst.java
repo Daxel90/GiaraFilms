ba7932e2b64a958aa701c3247b5e1f676137a73a
Giara <lockerz23@libero.it>
2015-11-29 23:01:08 +0100

    Dev PreRelese 14

--- GiaraFilms2.0/src/it/giara/TEst.java
+++ GiaraFilms2.0/src/it/giara/TEst.java
@@ -1,11 +1,22 @@
 package it.giara;
 
+import it.giara.analyze.enums.MainType;
+import it.giara.phases.Settings;
+import it.giara.phases.UpdateProgram;
+import it.giara.source.ListLoader;
+import it.giara.sql.SQL;
+import it.giara.sql.SQLQuery;
+import it.giara.syncdata.ServerQuery;
 import it.giara.tmdb.http.TMDBSearchTVSerie;
+import it.giara.utils.HttpPost;
+import it.giara.utils.Log;
 
 public class TEst
 {
 	public static void main(String[] args)
 	{
+		Log.log(Log.DEBUG, HttpPost.post("http://giaratest.altervista.org/giarafilms/MD5Update.php", "md5", "ccc"));
+		
 		
 		// HTTPSearchFilm test = new HTTPSearchFilm("spider");
 		
@@ -13,7 +24,20 @@ public class TEst
 		// HTTPFilmInfo("http://www.comingsoon.it/film/spider-man/1058/scheda/");
 		// res.getInfo();
 		
-		TMDBSearchTVSerie res = new TMDBSearchTVSerie("agent of");
+		SQL.connect();
+		Settings.init();
+		ListLoader.loadSources();
+		
+//		ServerQuery.sendScheda(SQLQuery.readScheda(674, MainType.Film));
+//		Log.log(Log.DEBUG, ServerQuery.requestScheda(674, MainType.Film));
+		
+//		ServerQuery.sendFileInfo("testtest", "350M",61387, MainType.Film );
+		
+		UpdateProgram.checkUpdate(null);
+		
+//		ServerQuery.loadUntil(Integer.parseInt(Settings.getParameter("lastserversync")));
+		
+//		TMDBSearchTVSerie res = new TMDBSearchTVSerie("agent of");
 		
 		// Log.log(Log.DEBUG, test.scheda.link);
 		
