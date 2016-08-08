56f7e9e249f6c9edb7d0b1159030911b33acced4
Giara <lockerz23@libero.it>
2016-08-08 23:07:13 +0200

    Added GZIP compression support

--- GiaraFilms2.0/src/it/giara/http/HTTPFileSources.java
+++ GiaraFilms2.0/src/it/giara/http/HTTPFileSources.java
@@ -1,9 +1,13 @@
 package it.giara.http;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.zip.GZIPInputStream;
 
 import it.giara.download.BotPackage;
 import it.giara.download.FileSources;
@@ -37,9 +41,45 @@ public class HTTPFileSources
 			URL url = new URL(site.replace("(search)", search));
 			final URLConnection conn = url.openConnection();
 			conn.setRequestProperty("User-Agent", "PoWeR-Script");
+			conn.setRequestProperty("accept-encoding", "gzip, deflate");
 			conn.setConnectTimeout(30000);
 			conn.connect();
-			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF8"));
+			
+			boolean gzipped = false;
+			
+			if(conn.getHeaderFields().containsKey("Content-Encoding"))
+			{
+				if(conn.getHeaderFields().get("Content-Encoding").contains("gzip"))
+				{
+					gzipped = true;
+				}
+			}
+			InputStream is = conn.getInputStream();
+			BufferedReader rd = null;
+			
+			if(gzipped)
+			{
+				Log.log(Log.DEBUG, "Zipped");
+				ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+				int nRead;
+				byte[] data = new byte[16384];
+				while ((nRead = is.read(data, 0, data.length)) != -1) {
+					buffer.write(data, 0, nRead);
+				}
+				buffer.flush();
+
+				byte[] compressedData = buffer.toByteArray();
+				Log.log(Log.DEBUG,"DataSize: "+compressedData.length);
+				GZIPInputStream gis = new GZIPInputStream(new ByteArrayInputStream(compressedData));
+				rd = new BufferedReader(new InputStreamReader(gis, "UTF-8"));
+			}
+			else
+			{
+				Log.log(Log.DEBUG, "Not Zipped");
+				rd = new BufferedReader(new InputStreamReader(is));
+			}
+			
+			
 			String line;
 			while ((line = rd.readLine()) != null)
 			{
